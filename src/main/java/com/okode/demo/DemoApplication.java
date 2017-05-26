cb2d0fc6cf91617aa6d249a40dbb02403d73d613
Pedro Jorquera <pjorquera@okode.com>
2017-05-26 17:53:00 +0200

    Refactor

--- src/main/java/com/okode/demo/DemoApplication.java
+++ src/main/java/com/okode/demo/DemoApplication.java
@@ -2,11 +2,28 @@ package com.okode.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication {
+@RestController
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+	
+	@GetMapping("/hello/{name}")
+	public String hello(@PathVariable String name) {
+		return "Hello, " + name;
+	}
+	
 }
